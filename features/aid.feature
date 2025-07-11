95bddce53ba118808c210e148d1a548057969525
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-07-11 03:03:51 +0200

    feat(features): test editing aid

---
+++ features/aid.feature
@@ -0,0 +1,88 @@
+Feature: Editing aid
+  # note: (format-mode-line) unfortunately requires C-based !noninteractive
+  #       github.com/emacs-mirror/emacs/blob/db46d5e/src/xdisp.c#L28617
+  @interactive-only
+  Scenario: Rows and columns are on the mode line
+    Given emacs loads
+    And temp buffer "rows-and-cols" contains "hello\nthere\n"
+
+    Then minor mode "line-number-mode" should be activated
+    And minor mode "column-number-mode" should be activated
+    And lighter in buffer "rows-and-cols" at "<point>" should show "<rowcol>":
+      |     point | rowcol |
+      |         1 | (1,0)  |
+      |         3 | (1,2)  |
+      |         6 | (1,5)  |
+      |         7 | (2,0)  |
+      |         9 | (2,2)  |
+      |        12 | (2,5)  |
+      |        13 | (3,0)  |
+      | point-max | (3,0)  |
+
+  Scenario: Overwriting selections
+    When emacs loads
+
+    Then minor mode "delete-selection-mode" should be activated
+    And minor mode "transient-mark-mode" should be activated
+
+    When temp buffer "overwriting" contains "hello\nthere"
+    And point in buffer "overwriting" is at "point-max"
+    # M-S-b
+    And I select previous word in buffer "overwriting"
+    Then mark in temp buffer "overwriting" should be active
+    And region in temp buffer "overwriting" should be active
+    And active region in temp buffer "overwriting" should select "((7 . 12))"
+
+    When I type "world" in buffer "overwriting"
+    Then temp buffer "overwriting" should contain "hello\nworld"
+
+  Scenario: Too long lines
+    Given emacs loads
+    # nitpick: cheap check, expand into an exact feature to allow lib swapping
+    Then minor mode "global-so-long-mode" should be activated
+
+  Scenario: Shortcuts for Org mode blocks
+    Given emacs loads
+    And temp buffer "org-shortcuts" contains ""
+    And mode "org-mode" is activated
+    And advice for "read-string" returns "ASK"
+
+    Then inserting "<text>" and pressing "TAB" should convert to:
+      | text | result                                                                   |
+      | <a   | #+begin_export ascii\nP\n#+end_export                                    |
+      | <c   | #+begin_center\nP\n#+end_center                                          |
+      | <e   | #+begin_example\nP\n#+end_example                                        |
+      | <h   | #+begin_export html\nP\n#+end_export                                     |
+      | <i   | #+index: P                                                               |
+      | <l   | #+begin_export latex\nP\n#+end_export                                    |
+      | <q   | #+begin_quote\nP\n#+end_quote                                            |
+      | <v   | #+begin_verse\nP\n#+end_verse                                            |
+      | <s   | #+name: ASK\n#+begin_src ASK :results output :exports both\nP\n#+end_src |
+
+  Scenario: Parenthesis pair is highlighted with point on
+    Given emacs loads
+
+    When temp buffer "<name>" contains "<contents>":
+      | name                     | contents |
+      | left-missing             | (        |
+      | missing-right            | )s       |
+      | left-right               | ()s      |
+      | left-left-right-missing  | (()s     |
+      | missing-left-right-right | ())s     |
+
+    # highlighting triggers at opening paren and one place after closing paren
+    Then temp buffer "<name>" in show-paren-mode should be highlighted by "<highlight>":
+      | name                     | highlight                        |
+      | left-missing             | 1:1:mismatch                     |
+      | missing-right            | 2:1:mismatch                     |
+      | left-right               | 1:1:match,3:2:match              |
+      | left-left-right-missing  | 1:1:mismatch,2:2:match,4:3:match |
+      | missing-left-right-right | 1:1:match,3:2:match,4:3:mismatch |
+
+  Scenario: Opening a pull request through remote reference
+    Given emacs loads
+    And temp buffer "open-pr" contains "Create a pull request\nremote: http://localhost\nremote:"
+    And point in buffer "open-pr" is at "point-max"
+
+    When I press "C-c C-o" in buffer "open-pr"
+    Then browser opens "http://localhost" url
---
+++ features/step-definitions/aid-steps.el
@@ -0,0 +1,192 @@
+;; -*- lexical-binding: nil; -*-
+(When "^emacs loads$"
+  (lambda ()
+    (should after-init-time)
+    (require 'elpaca)
+    (should elpaca-after-init-time)))
+
+(defvar aid-mock-calls nil)
+(defun aid-browse-mock (url)
+  (setq aid-mock-calls (push url aid-mock-calls)))
+(Before
+ (setq aid-mock-calls nil)
+ (advice-add 'browse-url :override #'aid-browse-mock))
+(After
+ (advice-remove 'browse-url #'aid-browse-mock))
+
+(When "^point in buffer \"\\([^\"]+\\)\" is at \"\\([^\"]+\\)\"$"
+  (lambda (buff-name pnt)
+    (with-current-buffer (get-buffer buff-name)
+      (if (string= "point-max" pnt)
+          (setq pnt (point-max))
+        (setq pnt (string-to-number pnt)))
+      (goto-char pnt))))
+
+(And "^I select previous word in buffer \"\\([^\"]+\\)\"$"
+  (lambda (buff-name)
+    (let ((buff (get-buffer buff-name)))
+      (with-current-buffer buff
+        (display-buffer buff)
+        (with-selected-window (get-buffer-window buff)
+          ;; note: requires display-buffer & with-selected-window
+          (execute-kbd-macro (kbd "M-S-b")))))))
+
+(Then "^mark in temp buffer \"\\([^\"]+\\)\" should be active$"
+  (lambda (buff-name)
+    (with-current-buffer buff-name
+      (should mark-active))))
+
+(Then "^region in temp buffer \"\\([^\"]+\\)\" should be active$"
+  (lambda (buff-name)
+    (with-current-buffer buff-name
+      (should (region-active-p)))))
+
+(Then "^active region in temp buffer \"\\([^\"]+\\)\" should select \"\\([^\"]+\\)\"$"
+  (lambda (buff-name region)
+    (let ((buff (get-buffer buff-name)))
+      (with-current-buffer buff
+        (display-buffer buff)
+        (with-selected-window (get-buffer-window buff)
+          (should transient-mark-mode)
+          ;; note: requires display-buffer & with-selected-window
+          (should (use-region-p))
+          (should (string= region (format "%s" (region-bounds)))))))))
+
+(And "^I type \"\\([^\"]+\\)\" in buffer \"\\([^\"]+\\)\"$"
+  (lambda (text buff-name)
+    (with-current-buffer (get-buffer buff-name)
+      (display-buffer buff-name)
+      (with-selected-window (get-buffer-window (get-buffer buff-name))
+        ;; note: requires display-buffer & with-selected-window
+        (execute-kbd-macro text)))))
+
+(And "^temp buffer \"\\([^\"]+\\)\" contains \"\\([^\"]+\\)\":$"
+  (lambda (buff-name contents data)
+    (let ((header (car data)))
+      (should (string-match-p (nth 0 header) buff-name))
+      (should (string-match-p (nth 1 header) contents)))
+
+    (dolist (item (cdr data))
+      (let ((buff-name (nth 0 item))
+            (contents (nth 1 item)))
+        (with-current-buffer (get-buffer-create buff-name)
+          (insert contents))))))
+
+(Then "^lighter in buffer \"\\([^\"]+\\)\" at \"\\([^\"]+\\)\" should show \"\\([^\"]+\\)\":$"
+  (lambda (buff-name pnt rowcol data)
+    (let ((header (car data)))
+      (should (string-match-p (nth 0 header) pnt))
+      (should (string-match-p (nth 1 header) rowcol)))
+
+    (with-current-buffer (get-buffer-create buff-name)
+      (display-buffer-in-side-window (get-buffer buff-name) '((side . bottom)))
+
+      (with-selected-window (get-buffer-window buff-name)
+      (dolist (item (cdr data))
+        (force-mode-line-update t)
+
+        (let ((pnt (nth 0 item))
+              (rowcol (nth 1 item)))
+          (if (string= "point-max" pnt)
+              (setq pnt (point-max))
+            (setq pnt (string-to-number pnt)))
+          (goto-char pnt)
+          (if (= pnt 0) (should (= 1 (point))) (should (= pnt (point))))
+
+          (let ((text (substring-no-properties
+                       (format-mode-line mode-line-position))))
+            (if (string-match "(\\([0-9]+\\),\\([0-9]+\\))" text)
+                (should (string= rowcol (match-string 0 text)))
+              (error "should never happen: %S" text)))))))))
+
+(And "^temp buffer \"\\([^\"]+\\)\" contains \"\\([^\"]*\\)\"$"
+  (lambda (buff-name contents)
+    (with-current-buffer (get-buffer-create buff-name)
+      (insert (string-replace "\\n" "\n" contents)))))
+
+(Then "^temp buffer \"\\([^\"]+\\)\" should contain \"\\([^\"]+\\)\"$"
+  (lambda (buff-name contents)
+    (with-current-buffer (get-buffer buff-name)
+      (should (string= (string-replace "\\n" "\n" contents)
+                       (buffer-string))))))
+
+(Then "^minor mode \"\\([^\"]+\\)\" should be activated$"
+  (lambda (mode-name)
+    (should (member 'delete-selection-mode minor-mode-list))))
+
+(Then "^temp buffer \"\\([^\"]+\\)\" in show-paren-mode should be highlighted by \"\\([^\"]+\\)\":$"
+  (lambda (buff-name highlight data)
+    (let ((header (car data)))
+      (should (string-match-p (nth 0 header) buff-name))
+      (should (string-match-p (nth 1 header) highlight)))
+
+    (dolist (item (cdr data))
+      (let ((item-buff-name (nth 0 item))
+            (item-highlight (nth 1 item)))
+        (with-current-buffer (get-buffer item-buff-name)
+          ;; (display-buffer-in-side-window (get-buffer item-buff-name) '((side . bottom)))
+          ;; (with-selected-window (get-buffer-window item-buff-name)
+          ;; (show-paren-mode)
+          (dolist (loc-point-color (string-split item-highlight ","))
+            (setq loc-point-color (string-split loc-point-color ":"))
+            (goto-char (string-to-number (car loc-point-color)))
+
+            (should show-paren-mode)
+            (should (catch 'found
+                      (dolist (item timer-idle-list)
+                        (when (eq 'show-paren-function (aref item 5))
+                          (throw 'found t)))))
+            ;; note(idle): tricky to trigger
+            (show-paren-function)
+
+            ;; example overlay list returned:
+            ;; (#<overlay from 1 to 2 in left-open-missing>)
+            (let* ((overlays
+                    (overlays-at (string-to-number (cadr loc-point-color)))))
+              (unless overlays
+                (error "should never happen: %s, %s" item-buff-name (point)))
+              (should (string= (format "show-paren-%s" (caddr loc-point-color))
+                               (overlay-get (car overlays) 'face))))))))))
+
+(When "^I press \"\\([^\"]+\\)\" in buffer \"\\([^\"]+\\)\"$"
+  (lambda (binding buff-name)
+    (with-current-buffer (get-buffer-create buff-name)
+      (let (proc)
+        (unwind-protect
+            (progn
+              (setq proc (start-process
+                          "abc" (get-buffer buff-name) "sleep" "2"))
+              (term-mode)
+              (should (term-in-line-mode))
+              (display-buffer-in-side-window
+               (get-buffer buff-name) '((side . bottom)))
+              (with-selected-window (get-buffer-window buff-name)
+                (should (fboundp 'my-open-pr))
+                (should (string= "C-c C-o" binding))
+                (should (lookup-key term-raw-map (kbd binding)))
+                (should (lookup-key (current-local-map) (kbd binding)))
+                (my-open-pr)))
+          (delete-process proc))))))
+
+;; missing check for C-x C-o in keybindings
+;; missing opening trigger and mock calls capturing
+(Then "^browser opens \"\\([^\"]+\\)\" url$"
+  (lambda (arg)
+    (should (string= arg (car aid-mock-calls)))
+    (should (= 1 (length aid-mock-calls)))))
+
+
+(And "^mode \"\\([^\"]+\\)\" is activated$"
+  (lambda (arg)
+
+    ))
+
+(And "^advice for \"\\([^\"]+\\)\" returns \"\\([^\"]+\\)\"$"
+  (lambda (arg-1 arg-2)
+
+    ))
+
+(Then "^inserting \"\\([^\"]+\\)\" and pressing \"\\([^\"]+\\)\" should convert to:$"
+  (lambda (arg-1 arg-2 arg-3)
+
+    ))
